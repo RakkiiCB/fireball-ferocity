c6ff01c2ae36aef0a8b40e9e6a30b2074dcf5cc4
jackbui05 <jackbui05@htomail.com>
2023-10-26 23:58:46 -0700

    Implement Movement and Animation States

---
+++ Fireball_Ferocity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+    private bool isFalling; // Parameter to track falling
+    private bool isLanding; // Parameter to track landing
+
+    private void Awake()
+    {
+        // Grabs references for rigidbody and animator from game object
+        anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+        // Lock the Z-axis rotation to keep the object upright.
+        body.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        // Flip player when facing left/right.
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+            Jump();
+
+        // Check if the player is falling
+        isFalling = body.velocity.y < 0;
+
+        // Set animation parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+        anim.SetBool("falling", isFalling);
+
+        // Handle landing logic
+        if (isFalling && !isLanding)
+        {
+            // Transition to landing state
+            anim.ResetTrigger("jump"); // Reset jump trigger if it was set
+            anim.SetTrigger("land");
+            isLanding = true;
+        }
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+            isLanding = false; // Reset landing when grounded
+        }
+    }
+}
