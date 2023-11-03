0585058f05d06a2fabb9be01d65befe5b88327c5
RAKKII-san <schuang2001@gmail.com>
2023-11-03 13:37:27 -0700

    Putting off assets for now, ready for prototype 2023-11-05

---
+++ Fireball_Ferocity/Assets/Scripts/EnemyGunner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGunner : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
