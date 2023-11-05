cf89594edb5e6e20c4977178b2cc84e55be267f5
RAKKII-san <schuang2001@gmail.com>
2023-11-04 20:07:42 -0700

    Bullet collision works

--- Fireball_Ferocity/Assets/Scripts/EnemyGunnerBullet.cs
+++ Fireball_Ferocity/Assets/Scripts/EnemyGunnerBullet.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class EnemyGunnerBullet : MonoBehaviour
 {
-    public playerHealth pHealth;
+    private GameObject player;
+    private Rigidbody2D rb;
     public float damage;
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if(timer > 10)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            pHealth.health -= damage;
+            other.gameObject.GetComponent<playerHealth>().health -= damage;
+            Debug.Log("bullet hit");
+            Debug.Log(other.gameObject.GetComponent<playerHealth>().health);
+            Destroy(gameObject);
         }
     }
 }
